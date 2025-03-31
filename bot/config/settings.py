dd91aa301105fa3c292cfae2cd7661ba346bf9c9
hubertszton1 <hubertszton@gmail.com>
2025-03-31 03:00:11 +0200

    Bot works but slowly

--- bot/config/settings.py
+++
@@ -1,7 +0,0 @@
-# Configuration settings for the Discord music bot
-
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-COMMAND_PREFIX = "!"  # Prefix for bot commands
-OWNER_ID = 1234567890  # Your Discord user ID
-DEFAULT_VOLUME = 0.5  # Default volume level for music playback
-SUPPORTED_AUDIO_FORMATS = ["mp3", "wav", "ogg"]  # Supported audio formats for playback
